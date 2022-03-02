73bff0e1fd1a5eecd0b93220661a71d2827d721c
vrvairavan92 <avrvairavan@gmail.com>
2022-03-02 17:19:17 +0530

    second commit

--- src/main/java/com/sample/SampleAutomation/SampleClass.java
+++ src/main/java/com/sample/SampleAutomation/SampleClass.java
@@ -5,6 +5,8 @@ public class SampleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Automation Engineer");
+		
+		System.out.println("Selenium Automation");
 
 	}
 
