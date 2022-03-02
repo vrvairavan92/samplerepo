d7c734e5a3ce004fcf906accf3be98bf96f2abad
vrvairavan92 <avrvairavan@gmail.com>
2022-03-02 13:30:00 +0530

    add my project

---
+++ src/main/java/com/sample/SampleAutomation/SampleClass.java
@@ -0,0 +1,11 @@
+package com.sample.SampleAutomation;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Automation Engineer");
+
+	}
+
+}
